933ace652343d3f64c5e5ad915c71079adcd24cd
shreekarsharma <shreekarsharma@hotmail.com>
2025-06-18 13:39:17 +0530

    UI improvements

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
-      <div className="md:px-56 py-56">
+      <div className="md:px-56 py-10">
         <div className="flex max-w-96 flex-col space-y-5 rounded-lg py-10 px-5 shadow-xl mx-auto">
           <div className="mx-auto mb-2 space-y-3">
             <h1 className="text-2xl sm:text-3xl font-bold text-gray-700">Log into NeoCommerce</h1>
@@ -15,7 +15,7 @@ const Login = () => {
               <input
                 type="text"
                 id="email1"
-                className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                className="border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                 placeholder=" "
               />
               <label
@@ -33,7 +33,7 @@ const Login = () => {
               <input
                 type="password"
                 id="password"
-                className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                className="border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                 placeholder=" "
               />
               <label
